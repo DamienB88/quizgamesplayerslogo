4563c902a559ace125628fe6c91f1674d495722e
DamienB88 <50745491+DamienB88@users.noreply.github.com>
2023-11-06 21:44:14 +0100

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,32 @@
+// SearchBar.js
+import React, { useState } from 'react';
+
+const SearchBar = ({ onGuessSubmit }) => {
+    const [userGuess, setUserGuess] = useState('');
+
+    const handleInputChange = (event) => {
+        setUserGuess(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onGuessSubmit(userGuess);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Your guess"
+                    value={userGuess}
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Submit Guess</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchBar;
 
