7049e45305118fb04de11328222216753b68b67e
DamienB88 <50745491+DamienB88@users.noreply.github.com>
2023-11-06 21:42:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Quiz from './Quiz'; // Import your Quiz component
 
+ReactDOM.render(
+    <React.StrictMode>
+        <Quiz />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
